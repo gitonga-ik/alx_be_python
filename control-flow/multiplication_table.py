04bd14dc1a4c1096989a13a080f3b837bdf599ae
Ian Gitonga <139679065+gitonga-ik@users.noreply.github.com>
2025-09-19 14:57:35 +0300

    Control flow excercises

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,4 @@
+number = int(input("Enter number to see multiplication table:"))
+
+for current in range(1,11):
+    print(f"{number} * {current} = {number * current}")
---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,7 @@
+size = int(input("Enter size of pattern:"))
+row = 0
+
+while row < size:
+    print("*" * size)
+    row += 1
+    
