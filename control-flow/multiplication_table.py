b446418c8f3754bb3776f87cdfc781d5e121ec4a
Ian Gitonga <139679065+gitonga-ik@users.noreply.github.com>
2025-09-19 15:01:55 +0300

    Corrected prompts

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
-number = int(input("Enter number to see multiplication table:"))
+number = int(input("Enter a number to see its multiplication table:"))
 
 for current in range(1,11):
     print(f"{number} * {current} = {number * current}")
