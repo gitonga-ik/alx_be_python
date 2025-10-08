57402a3ebadb4003cded0432ba1f93ccc6cf7aab
Ian Gitonga <139679065+gitonga-ik@users.noreply.github.com>
2025-10-08 15:03:44 +0300

    Corrections

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,16 +3,24 @@ class Book:
         self.title = title
         self.author = author 
 
+    def __str__(self):
+        return f"{self.__class__.__name__}: {self.title} by {self.author}"
+
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        f"{self.__class__.__name__}: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        f"{self.__class__.__name__}: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
@@ -23,9 +31,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            if isinstance(book, EBook): 
-                print(f"{book.__class__.__name__}: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            elif isinstance(book, PrintBook): 
-                print(f"{book.__class__.__name__}: {book.title} by {book.author}, Page Count: {book.page_count}")
-            else: 
-                print(f"{book.__class__.__name__}: {book.title} by {book.author}")
+            print(book)
